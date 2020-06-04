beb7394829c0070e60ac15cc5928a36af51a0e08
unknown <manjiri.vadujkar@gmail.com>
2020-06-04 21:43:21 +0530

    Updates

---
+++ precache-manifest.d30785a636211062e4b08baf9f14fcd4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95b20f067b75862df08179b064b924f3",
+    "url": "/Face-Recognition-Brain/index.html"
+  },
+  {
+    "revision": "c48d3a13fc6743377c3e",
+    "url": "/Face-Recognition-Brain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b7cce580f887d6de02c1",
+    "url": "/Face-Recognition-Brain/static/css/main.02e4e497.chunk.css"
+  },
+  {
+    "revision": "c48d3a13fc6743377c3e",
+    "url": "/Face-Recognition-Brain/static/js/2.481490f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Face-Recognition-Brain/static/js/2.481490f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7cce580f887d6de02c1",
+    "url": "/Face-Recognition-Brain/static/js/main.e6348c58.chunk.js"
+  },
+  {
+    "revision": "377fdd6ebcb8a64a3a83",
+    "url": "/Face-Recognition-Brain/static/js/runtime-main.835e4b00.js"
+  }
+]);
