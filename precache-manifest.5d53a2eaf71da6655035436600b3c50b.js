656550f9c26f3285db31e5e2fce41db2757e779b
unknown <manjiri.vadujkar@gmail.com>
2020-06-04 21:39:50 +0530

    Updates

---
+++ precache-manifest.5d53a2eaf71da6655035436600b3c50b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5b731f0a342439f01fe2bfb04a12bdd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0e00fa04a28f24343b2",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3b71820b5c24e4681fe7",
+    "url": "/static/css/main.02e4e497.chunk.css"
+  },
+  {
+    "revision": "a0e00fa04a28f24343b2",
+    "url": "/static/js/2.7a7f90e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7a7f90e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b71820b5c24e4681fe7",
+    "url": "/static/js/main.bc5e78c6.chunk.js"
+  },
+  {
+    "revision": "b83cbc8f1b0516bb5a45",
+    "url": "/static/js/runtime-main.39d042f1.js"
+  }
+]);
